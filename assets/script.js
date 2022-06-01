1e5d7a4fdd1f8cfabb7f8ec160c302d7fbf96e98
Olivia McKee <olivia.mckee97@gmail.com>
2022-06-01 12:39:06 -0500

    save to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var taskArray = ["", "", "", "", "", "", "", "", ""];
+//  var taskArray = ["", "", "", "", "", "", "", "", ""];
 
 // display current date and time at tope of page
 var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm a");
@@ -11,20 +11,36 @@ $(".description").click(function() {
 
 // save task description to local storage on click
 $(".saveBtn").click(function() {
-    var tasks = $(this).siblings(".description");
+    var tasks = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id");
     // console.log(tasks.data().timeSlot);
-    taskArray[tasks.data().timeSlot - 9] = tasks.text();
-    localStorage.setItem("tasks", JSON.stringify(taskArray));
+    // taskArray[tasks.data().timeSlot - 9] = tasks.text();
+    // localStorage.setItem("tasks", JSON.stringify(taskArray));
+    localStorage.setItem(hour, tasks);
 })
 
-// load tasks from local storage
+//load tasks from local storage
 $(document).ready(function() {
-    taskArray = JSON.parse(localStorage.getItem("tasks"));
-    console.log(taskArray);
-    for (let i = 0; i < taskArray.length; i++) {
-        console.log(taskArray[i]);
-        $(".description").text(taskArray[i]); //why doesnt this work????
-    };
+    // taskArray = JSON.parse(localStorage.getItem("tasks"));
+    // console.log(taskArray);
+    // for (let i = 0; i < taskArray.length; i++) {
+    //     console.log(taskArray[i]);
+    //     $(".description").text(taskArray[i]); //why doesnt this work????
+    // };
+    
+    // var ten = localStorage.getItem("ten");
+    // var eleven = localStorage.getItem("eleven");
+    // var twelve = localStorage.getItem("twelve");
+    // var one = localStorage.getItem("one");
+    // var two = localStorage.getItem("two");
+    // var three = localStorage.getItem("three");
+    // var four = localStorage.getItem("four");
+    // var five = localStorage.getItem("five");
+
+    var nine = localStorage.getItem("nine");
+    console.log(nine);
+    console.log($("#nine").siblings(".description"));
+    $("#nine").siblings(".description").val(localStorage.getItem("nine"));
 })
     //copy start loop for or for each loop 
 
